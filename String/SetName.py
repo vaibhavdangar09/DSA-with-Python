4275a09353494f9211cad5c2532bed85d763951a
anuj vaghani <anujvaghani0@gmail.com>
2023-10-01 10:41:39 +0530

    added

---
+++ String/SetName.py
@@ -0,0 +1,8 @@
+def set_name(str):
+    capitalized_string = str.title()
+    string_without_spaces = str.replace(" ", "")
+    print(string_without_spaces)
+
+
+str = "Excel Sheet Column Title"
+set_name(str)
