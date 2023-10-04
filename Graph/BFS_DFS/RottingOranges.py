c779c6a22a3a0610de359b450ec6d206a6bde2a1
Darshan Ambaliya <95023360+DarshanAmbaliya@users.noreply.github.com>
2023-10-04 18:26:09 +0530

    Update RottingOranges.py

--- Graph/BFS_DFS/RottingOranges.py
+++ Graph/BFS_DFS/RottingOranges.py
@@ -39,4 +39,4 @@ class Solution:
         return time
 
 sol = Solution()
-print(sol.orangesRotting([[2, 1, 1], [1, 1, 0], [0, 1, 1]]))
+print(sol.orangesRotting([[2, 1, 1], [1, 1, 0], [0, 1, 1],[1,1,0]))
